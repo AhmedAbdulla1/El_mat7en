4335a3eb611c17a20c748b9bd5ccd4b3026611d7
KhaledElalfy1 <khaledalfy53@gmail.com>
2023-09-18 09:35:36 -0700

    feed form done

--- lib/presentation/feed_form/feed_form.dart
+++ lib/presentation/feed_form/feed_form.dart
@@ -5,29 +5,99 @@ import 'package:tanta_app/presentation/common/reusable/custom_scaffold.dart';
 import 'package:tanta_app/presentation/feed_form/widgets/custom_text_form_filed.dart';
 import 'package:tanta_app/presentation/feed_states/widgets/custom_app_bar.dart';
 
-
-class FeedForm extends StatelessWidget {
+class FeedForm extends StatefulWidget {
   const FeedForm({super.key});
+
+  @override
+  State<FeedForm> createState() => _FeedFormState();
+}
+
+class _FeedFormState extends State<FeedForm> {
+  TextEditingController name = TextEditingController(),
+      nationalId = TextEditingController(),
+      village = TextEditingController(),
+      center = TextEditingController(),
+      government = TextEditingController(),
+      phone = TextEditingController();
+  @override
+  void dispose() {
+    name.dispose();
+    nationalId.dispose();
+    village.dispose();
+    center.dispose();
+    government.dispose();
+    phone.dispose();
+    super.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return customScaffold(
         body: SafeArea(
-            child: Column(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        const CustomAppBar(
-          title: 'تقديم الطلب',
-        ),
-        const CustomTextFormFiled(hintText: 'الاسم'),
-        const CustomTextFormFiled(hintText: 'الرقم القومى'),
-        const CustomTextFormFiled(hintText: 'العنوان'),
-        const CustomTextFormFiled(hintText: 'رقم التليفون'),
-        customElevatedButtonWithoutStream(onPressed: () {}, text: 'طلب'),
-        SizedBox(
-          height: 20.h,
-        ),
-      ],
+            child: Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20.w),
+      child: ListView(
+        // mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          const CustomAppBar(
+            title: 'تقديم الطلب',
+          ),
+          SizedBox(
+            height: 30.h,
+          ),
+          CustomTextFormFiled(hintText: 'الاسم', textEditingController: name),
+          SizedBox(
+            height: 20.h,
+          ),
+          CustomTextFormFiled(
+              hintText: 'الرقم القومى', textEditingController: nationalId),
+          SizedBox(
+            height: 20.h,
+          ),
+          Row(
+            textDirection: TextDirection.rtl,
+            children: [
+              SizedBox(
+                height: 100.h,
+                width: 110.w,
+                child: CustomTextFormFiled(
+                    hintText: 'قرية', textEditingController: village),
+              ),
+              SizedBox(
+                width: 10.w,
+              ),
+              SizedBox(
+                height: 100.h,
+                width: 110.w,
+                child: CustomTextFormFiled(
+                    hintText: 'مركز', textEditingController: center),
+              ),
+              SizedBox(
+                width: 10.w,
+              ),
+              SizedBox(
+                height: 100.h,
+                width: 110.w,
+                child: CustomTextFormFiled(
+                    hintText: 'محافظة', textEditingController: government),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20.h,
+          ),
+          CustomTextFormFiled(
+              hintText: 'رقم التليفون', textEditingController: phone),
+          SizedBox(
+            height: 70.h,
+          ),
+          customElevatedButtonWithoutStream(onPressed: () {}, text: 'طلب'),
+          SizedBox(
+            height: 20.h,
+          ),
+        ],
+      ),
     )));
   }
 }
--- lib/presentation/feed_form/widgets/custom_text_form_filed.dart
+++ lib/presentation/feed_form/widgets/custom_text_form_filed.dart
@@ -3,13 +3,15 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tanta_app/presentation/resources/font_manager.dart';
 
 class CustomTextFormFiled extends StatelessWidget {
-  const CustomTextFormFiled({super.key, required this.hintText});
+  const CustomTextFormFiled({super.key, required this.hintText, required this.textEditingController});
   final String hintText;
+  final TextEditingController textEditingController;
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 36.w, vertical: 10.h),
+      padding: EdgeInsets.symmetric(vertical: 10.h),
       child: TextFormField(
+        controller:textEditingController ,
         textAlign: TextAlign.center,
         decoration: InputDecoration(
           hintText: hintText,
