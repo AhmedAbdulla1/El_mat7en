63e01b2a92c0aa44d200bb63c579879e2e61b320
KhaledElalfy1 <khaledalfy53@gmail.com>
2023-09-18 23:59:12 -0700

    add custom widget

--- lib/presentation/administrative/administrative.dart
+++ lib/presentation/administrative/administrative.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tanta_app/presentation/administrative/widgets/custom_data_show_container.dart';
 import 'package:tanta_app/presentation/common/reusable/custom_scaffold.dart';
 import 'package:tanta_app/presentation/feed_form/widgets/custom_text_form_filed.dart';
 import 'package:tanta_app/presentation/feed_states/widgets/custom_app_bar.dart';
-import 'package:tanta_app/presentation/resources/color_manager.dart';
-import 'package:tanta_app/presentation/resources/font_manager.dart';
+
 
 class Administrative extends StatefulWidget {
   const Administrative({super.key});
@@ -98,35 +98,3 @@ class _AdministrativeState extends State<Administrative> {
   }
 }
 
-class CustomDataShowContainer extends StatelessWidget {
-  const CustomDataShowContainer({super.key, required this.title});
-  final String title;
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 8.w),
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 16.w),
-        constraints: BoxConstraints(minWidth: 50.w),
-        alignment: Alignment.center,
-        // width: 170.w,
-        height: 55.h,
-        decoration: BoxDecoration(
-          color: Colors.transparent,
-          borderRadius: BorderRadius.circular(50.r),
-          border: Border.all(
-            color: ColorManager.primary,
-            width: 4,
-          ),
-        ),
-        child: Text(
-          title,
-          style: TextStyle(
-            color: const Color(0xff7D7D7D),
-            fontSize: FontSize.s22,
-          ),
-        ),
-      ),
-    );
-  }
-}
